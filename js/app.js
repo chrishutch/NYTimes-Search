09154b4f3d9ca170e7052fa6901e84bb2ac981da
Chris Hutchinson <chrisbond19@me.com>
2018-01-10 17:08:20 -0600

    Fixed title and image icon

--- js/app.js
+++ js/app.js
@@ -3,9 +3,7 @@ $(document).ready(function(){
 	event.preventDefault();
 	$('#searchHeading').text('Search Results:');
 	var wordSubmit = $('#searchTerm').val();
-	console.log($('#searchTerm').val());
 	var articleNumber = $('#inputState').val();
-	console.log($('#inputState').val());
 	var beginDate = $('#startYear').val();
 	var endDate = $('#endYear').val();
 	
@@ -34,12 +32,6 @@ $(document).ready(function(){
 			  	summary.text(result.response.docs[i].snippet);
 			  	well.append(html).append(summary);
 			  	$('#articles').append(well.append(html).append(summary));
-			  	console.log(html);
-			  	console.log(result.response.docs[i].web_url);
-			  	console.log(result.response.docs[i].headline.main);
-			  	console.log(result.response.docs[i].pub_date);
-			   	console.log(result.response.docs[i].snippet);
-
 			  }
 			  
 			})
