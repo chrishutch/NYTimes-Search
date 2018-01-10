f0eb8f547505c706976e6002d044c697abc50dc3
Chris Hutchinson <chrisbond19@me.com>
2018-01-10 10:35:57 -0600

    Added HTML, CSS and javascript files

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$('#searchButton').on('click',function(){
+	event.preventDefault();
+
+	var wordSubmit = $('#searchTerm').val();
+	console.log($('#searchTerm').val());
+	var articleNumber = $('#inputState').val();
+	var beginDate = $('#startYear').val();
+	var endDate = $('#endYear').val();
+	
+      var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+			url += '?' + $.param({
+			  'api-key': "b7d02bc603134c68bc2d1b24e8849da3",
+			  'q': wordSubmit,
+			  'fq': articleNumber,
+			  'begin_date': beginDate+'0101',
+  			  'end_date': endDate+'0101'
+			});
+
+			$.ajax({
+			  url: url,
+			  method: 'GET'
+			}).then(function(result) {
+			  console.log(result);
+			  for (var i = 0; i < result.response.docs.length; i++){
+			  	var html = $('<a>');
+			  	html.attr('href',result.response.docs[i].web_url);
+			  	html.text(result.response.docs[i].headline.main);
+			  	$('#articles').append(html);
+			  	console.log(result.response.docs[i].web_url);
+			  	console.log(result.response.docs[i].headline.main);
+			  	console.log(result.response.docs[i].pub_date);
+			  }
+			  
+			})
+			});
+});
